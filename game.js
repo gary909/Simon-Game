a38a387d7fadee399352bc696a83d09a3da53e80
gary <gary.white@lodgit.com>
2021-12-14 16:47:22 +0100

    random colours added

--- game.js
+++ game.js
@@ -1,10 +1,16 @@
 // alert("Hello!");
 // $("h1").css("color", "green"); Test to see if jQuery is installed ok > working
 
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+let randomChosenColour = nextSequence();
+
+
 function nextSequence(){
-    let ranNum = (Math.random() * 3) + 1;
+    let ranNum = (Math.random() * 4);
     randomNumber = Math.floor(ranNum) // return a random num between 1 - 3
     return randomNumber;
 };
 
-console.log(nextSequence());
+console.log(buttonColours[randomChosenColour]);
+// console.log(randomChosenColour);
