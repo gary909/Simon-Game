6a3293bc0ea2e5195a8a263b41710572b2692d58
gary <gary.white@lodgit.com>
2021-12-14 16:38:10 +0100

    random number generator added

--- game.js
+++ game.js
@@ -1,2 +1,10 @@
 // alert("Hello!");
 // $("h1").css("color", "green"); Test to see if jQuery is installed ok > working
+
+function nextSequence(){
+    let ranNum = (Math.random() * 3) + 1;
+    randomNumber = Math.floor(ranNum) // return a random num between 1 - 3
+    return randomNumber;
+};
+
+console.log(nextSequence());
