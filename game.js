38c918dc2d2169b5bfccd47d19dc3b269a5d467c
gary <gary.white@lodgit.com>
2021-12-16 16:12:40 +0100

    code

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 // alert("Hello!");
 // $("h1").css("color", "green"); // Test to see if jQuery is installed ok > working
-$("#red").css("background-color", "green"); // select the button with the ID 'red' and change it's BG color green
-
+// $("#red").css("background-color", "green"); // select the button with the ID 'red' and change it's BG color green
+// $("#red").fadeIn(1000).fadeOut(1000).fadeIn(1000);
 
 let buttonColours = ["red", "blue", "green", "yellow"];
 
@@ -12,8 +12,10 @@ function nextSequence(){
     randomNumber = Math.floor(ranNum) // return a random num between 0 - 3
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    //return gamePattern;
-    // $("#{{randomChosenColour}}").css("background-color", "green");
+
+    $("#" + randomChosenColour).fadeIn(200).fadeOut(200).fadeIn(200);
+
+    // return gamePattern;
 };
 
 
