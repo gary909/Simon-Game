253169dbd2b8f6afd6fd4a589d097515661dfb9c
gary <gary.white@lodgit.com>
2022-01-04 16:56:45 +0100

    code

--- game.js
+++ game.js
@@ -1,54 +1,32 @@
-// alert("Hello!");
-// $("h1").css("color", "green"); // Test to see if jQuery is installed ok > working
-// $("#red").css("background-color", "green"); // select the button with the ID 'red' and change it's BG color green
-// $("#red").fadeIn(1000).fadeOut(1000).fadeIn(1000);
 
-let buttonColours = ["red", "blue", "green", "yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-let gamePattern = [];
+var gamePattern = [];
 
-let userClickedPattern = [];
+//3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
+var userClickedPattern = [];
 
-function nextSequence(){
-    let ranNum = (Math.random() * 4);
-    randomNumber = Math.floor(ranNum) // return a random num between 0 - 3
-    let randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
-    
-    $("#" + randomChosenColour).fadeIn(200).fadeOut(200).fadeIn(200);
-    
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+$(".btn").click(function() {
 
-    //Play audio onClick
+  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+  var userChosenColour = $(this).attr("id");
 
-    $("#green").click(function(){ // onClick
-        audio.play();
-        $("#green").fadeIn(200).fadeOut(200).fadeIn(200);
-        console.log("Green was clicked");
-    });
+  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+  userClickedPattern.push(userChosenColour);
 
-    $("#red").click(function(){ // onClick
-        audio.play();
-        $("#red").fadeIn(200).fadeOut(200).fadeIn(200);
-        console.log("Red was clicked");
-    });
+  console.log(userClickedPattern);
 
-    $("#yellow").click(function(){ // onClick
-        audio.play();
-        $("#yellow").fadeIn(200).fadeOut(200).fadeIn(200);
-        console.log("Yellow was clicked");
-    });
+});
 
-    $("#blue").click(function(){ // onClick
-        audio.play();
-        $("#blue").fadeIn(200).fadeOut(200).fadeIn(200);
-        console.log("Blue was clicked");
-    });
-    // return gamePattern;
-};
+function nextSequence() {
 
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-console.log(nextSequence());
-// console.log(randomChosenColour);
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
