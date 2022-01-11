16dff700cff4ecd3a5a043da847db462ab2aa1b3
gary <gary.white@lodgit.com>
2022-01-11 17:11:02 +0100

    code working - section 8

--- game.js
+++ game.js
@@ -5,9 +5,7 @@ var gamePattern = [];
 var userClickedPattern = [];
 
 var started = false;
-
 var level = 0;
-
 
 //listen for any keypress
 $(document).keypress(function(event){
@@ -19,7 +17,6 @@ $(document).keypress(function(event){
   }
 });
 
-
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
@@ -28,22 +25,46 @@ $(".btn").click(function() {
   console.log(userClickedPattern);
   playSound(userChosenColour);
   animatePress(userChosenColour);
-  checkAnswer(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
 });
+
+function checkAnswer(currentLevel){
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    
+    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+    if (userClickedPattern.length === gamePattern.length){
+
+      //5. Call nextSequence() after a 1000 millisecond delay.
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+
+    }
+
+  } else {
+    console.log("wrong");
+  }
+}
+
+
+
 
 
 function nextSequence() {
+
+  //6. Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+  userClickedPattern = [];
   
   level += 1;
 
   $("#level-title").text("level " + level); // change text
-  
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  
+
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  
   playSound(randomChosenColour);
   // console.log('hellloooo');
 }
@@ -54,20 +75,10 @@ function playSound(name) {
 }
 
 function animatePress(currentColour) {
-
   $("#" + currentColour).addClass("pressed");
-  
   // console.log("You pressed " + currentColour);
-
   setTimeout(function () {
     $("#" + currentColour).removeClass("pressed");
   }, 100);
 }
 
-function checkAnswer(currentLevel){
-  if (currentLevel == gamePattern) {
-    console.log("success")
-  } else {
-    console.log("wrong")
-  }
-}
